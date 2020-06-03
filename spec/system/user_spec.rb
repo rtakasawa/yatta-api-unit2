4884f5a41e9fa13d643c450d024b19676f4e6329
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-03 14:28:59 +0900

    会員編集・退会機能

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe  "ユーザー登録・ログイン・ログアウト機能", typ
         fill_in "user[password]", with: '0000000'
         fill_in "user[password_confirmation]", with: '0000000'
         click_on "アカウント登録"
-        expect(current_path).to eq root_path
+        expect(current_path).to eq materials_path
       end
       it "ログインせずに教材一覧画面に行くと、ログイン画面に飛ぶ" do
         visit materials_path
@@ -35,18 +35,40 @@ RSpec.describe  "ユーザー登録・ログイン・ログアウト機能", typ
       it "ログインできること" do
         expect(current_path).to eq root_path
       end
-      it "自分の詳細画面(マイページ)に飛べること" do
-        click_on "会員情報"
-        expect(current_path).to eq user_path(id: 1)
-      end
-      it "自分の情報の編集画面に飛び、編集できること" do
-        click_on "マイページ"
-        expect(current_path).to eq user_path(id: 1)
-      end
       it "ログアウトができること" do
         click_on "ログアウト"
         expect(current_path).to eq new_user_session_path
       end
+      it "自分の詳細画面(マイページ)に飛べること" do
+        click_on "会員情報"
+        expect(current_path).to eq user_path(id: 1)
+      end
+      it "自分の情報の編集・退会画面に飛べること" do
+        click_on "会員情報"
+        click_on "会員編集・退会"
+        expect(current_path).to eq edit_user_registration_path(1)
+      end
+      it "自分の情報を編集できること" do
+        click_on "会員情報"
+        click_on "会員編集・退会"
+        fill_in "user[name]", with: "sample1"
+        fill_in "user[email]", with: "sample1@example.com"
+        fill_in "user[password]", with: '1111111'
+        fill_in "user[password_confirmation]", with: '1111111'
+        fill_in "user[current_password]", with: '0000000'
+        click_on "更新"
+        expect(current_path).to eq user_path(id: 1)
+        expect(page).to have_content "sample1"
+      end
+      it "退会できること" do
+        click_on "会員情報"
+        click_on "会員編集・退会"
+        click_on "アカウント削除"
+        page.accept_confirm
+        expect(current_path).to eq new_user_session_path
+        expect(User.count).to eq 0
+      end
+
       # it "一般ユーザが他人の詳細画面に飛ぶとタスク一覧ページに遷移すること" do
       #   visit user_path(id: 2)
       #   expect(current_path).to eq tasks_path
