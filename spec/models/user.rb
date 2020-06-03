e8de74496e2659ecf0b00aaf1c9b3c8650c5f5e2
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-03 21:52:28 +0900

    教材テーブルの作成

--- spec/models/user.rb
+++ spec/models/user.rb
@@ -1,33 +1,35 @@
 require 'rails_helper'
 require 'devise'
 
-RSpec.describe  "ユーザー登録機能" do
-  it "nameが空ならバリデーションが通らない" do
-    user = User.new(name: "", email: "sample@example.com", password: "0000000")
-    expect(user).not_to be_valid
-  end
-  it "emailが空ならバリデーションが通らない" do
-    user = User.new(name: "sample", email: "", password: "0000000")
-    expect(user).not_to be_valid
-  end
-  it "emailが既に登録済みの場合、バリデーションが通らない" do
-    FactoryBot.create(:user)
-    user = User.new(name: "sample_2", email: "sample@example.com", password: "0000000")
-    expect(user).not_to be_valid
-  end
-  it "emailの入力形式が適切でない場合、バリデーションが通らない" do
-    user = User.new(name: "sample", email: "aaaaaaaa", password: "0000000")
-    expect(user).not_to be_valid
-  end
-  it "passwordが空ならバリデーションが通らない" do
-    user = User.new(name: "sample", email: "sample@example.com", password: "")
-    expect(user).not_to be_valid
-  end
-  it "passwordが6文字未満ならバリデーションが通らない" do
-    user = User.new(name: "sample", email: "sample@example.com", password: "aaaaa")
-    expect(user).not_to be_valid
-  end
+RSpec.describe  "ユーザー関連" do
 
+  describe "ユーザー登録のテスト" do
+    it "nameが空ならバリデーションが通らない" do
+      user = User.new(name: "", email: "sample@example.com", password: "0000000")
+      expect(user).not_to be_valid
+    end
+    it "emailが空ならバリデーションが通らない" do
+      user = User.new(name: "sample", email: "", password: "0000000")
+      expect(user).not_to be_valid
+    end
+    it "emailが既に登録済みの場合、バリデーションが通らない" do
+      FactoryBot.create(:user)
+      user = User.new(name: "sample_2", email: "sample@example.com", password: "0000000")
+      expect(user).not_to be_valid
+    end
+    it "emailの入力形式が適切でない場合、バリデーションが通らない" do
+      user = User.new(name: "sample", email: "aaaaaaaa", password: "0000000")
+      expect(user).not_to be_valid
+    end
+    it "passwordが空ならバリデーションが通らない" do
+      user = User.new(name: "sample", email: "sample@example.com", password: "")
+      expect(user).not_to be_valid
+    end
+    it "passwordが6文字未満ならバリデーションが通らない" do
+      user = User.new(name: "sample", email: "sample@example.com", password: "aaaaa")
+      expect(user).not_to be_valid
+    end
+  end
   # context "管理者が一人の場合" do
   #   it "管理者のユーザー登録は削除できない（メソッドが呼び出されているか）" do
   #     user = create(:admin_user)
