815537f81c4d2020124ccefaf2479e8f51c02df3
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-03 23:44:23 +0900

    教材の新規投稿,pathのバリデーション実装

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,6 +3,6 @@ class Material < ApplicationRecord
 
   validates :title, presence: true
   validates :category, presence: true
-
+  validates :path, format: /\A#{URI::regexp(%w(http https))}\z/
   enum category:{ book: 0, video: 1, web: 2, other: 3 }
 end
