134da9b7b71e9c76e8044917db9451e3a8987c1f
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-03 14:28:59 +0900

    会員編集・退会機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'materials#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :materials
   resources :users, only: [:show]
 end
