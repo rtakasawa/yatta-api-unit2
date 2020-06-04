e0b92efbd1a801bacf3f228a12be10e218976fe8
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-04 10:36:37 +0900

    gem rinku導入前

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,6 +3,7 @@ class Material < ApplicationRecord
 
   validates :title, presence: true
   validates :category, presence: true
+  # allow_blank: true:入力がブランク時はバリデーションをスキップ
   validates :path, format: /\A#{URI::regexp(%w(http https))}\z/, allow_blank: true
   enum category:{ book: 0, video: 1, web: 2, other: 3 }
 end
