1767f0ab337ae37974dde72ce24853e63a5ed4b9
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-04 16:19:14 +0900

    教材検索機能（書籍）実装完了（仮）

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -29,13 +29,26 @@ class MaterialsController < ApplicationController
 
   def edit; end
 
+  def update
+    if @material.update(material_params)
+      redirect_to materials_path, info: "教材情報は更新されました"
+    else
+      render "edit"
+    end
+  end
+
   def destroy
     @material.destroy
     redirect_to materials_path, info: "教材は削除されました"
   end
 
+  # 楽天APIから書籍情報を取得
+  # 本のタイトルで検索。検索条件は不明確だが、おそらく曖昧検索
+  # 今はデフォルトの30件しか検索情報が取得できない
+  # ページネーション追加後に取得情報を増やせるか確認する
+  # ２文字以上検索ワードを入れないとエラーになる。とりあえずビューで制約した
   def book_search
-    if params[:keyword]
+    if params[:keyword].size
       @books = RakutenWebService::Books::Book.search(title: params[:keyword])
     end
   end
