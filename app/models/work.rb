30cc36fc675827265765b8558af0d12d0fd3bd4f
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-05 17:31:20 +0900

    workモデル：バリデーション、enum設定

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,10 @@
 class Work < ApplicationRecord
+  belongs_to :material
+
+  validates :start, presence: true
+  validates :end, presence: true
+  validates :status, presence: true
+
+  enum category:{ incomplete: 0, complete: 1 }
+
 end
