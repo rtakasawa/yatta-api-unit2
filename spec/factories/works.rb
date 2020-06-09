aa21c96615b5f9bc81c3dcc9c37e91dc8cb028a2
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-09 17:35:39 +0900

    work関連を実装※絞り込み関連は後で実装する

--- spec/factories/works.rb
+++ spec/factories/works.rb
@@ -1,29 +1,26 @@
-# FactoryBot.define do
-#   factory :work do
-#     id { 1 }
-#     start { "1" }
-#     end { "2" }
-#     category { "book" }
-#     path { "http://example.com" }
-#     note { "test_note1" }
-#     association :material
-#   end
-#   factory :second_work, class: Work do
-#     id { 2 }
-#     title { "test2" }
-#     author { "test_author2" }
-#     category { "video" }
-#     path { "http://example2.com" }
-#     note { "test_note2" }
-#     association :material
-#   end
-#   factory :third_work, class: Work do
-#     id { 3 }
-#     title { "test3" }
-#     author { "test_author3" }
-#     category { "web" }
-#     path { "http://example3.com" }
-#     note { "test_note3" }
-#     association :material
-#   end
-# end
+FactoryBot.define do
+  factory :work do
+    id { 1 }
+    start { "1" }
+    finish { "10" }
+    content { "難しい" }
+    status { "incomplete" }
+    association :material
+  end
+  factory :second_work, class: Work do
+    id { 2 }
+    start { "1-1" }
+    finish { "1-10" }
+    content { "復習必要" }
+    status { "complete" }
+    association :material
+  end
+  factory :third_work, class: Work do
+    id { 3 }
+    start { "１" }
+    finish { "１００" }
+    content { "よくできた" }
+    status { "incomplete" }
+    association :material
+  end
+end
