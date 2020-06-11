4b39c6d4dbe3e30d87fc29f1867d3177f05e8a65
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-11 18:24:29 +0900

    work/materialのfactorybotの修正

--- spec/factories/materials.rb
+++ spec/factories/materials.rb
@@ -2,28 +2,28 @@ FactoryBot.define do
   factory :material do
     id { 1 }
     title { "test1" }
-    author { "test_author1" }
     category { "book" }
     path { "http://example.com" }
     note { "test_note1" }
+    status { "learning" }
     association :user
   end
   factory :second_material, class: Material do
     id { 2 }
     title { "test2" }
-    author { "test_author2" }
     category { "video" }
     path { "http://example2.com" }
     note { "test_note2" }
+    status { "complete" }
     association :user
   end
   factory :third_material, class: Material do
     id { 3 }
     title { "test3" }
-    author { "test_author3" }
     category { "web" }
     path { "http://example3.com" }
     note { "test_note3" }
+    status { "learning" }
     association :user
   end
 end
