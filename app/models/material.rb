476a03f8812847d5c88e96a42a1f40521eff7f7d
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-11 18:14:19 +0900

    material,workモデルのバリデーション修正

--- app/models/material.rb
+++ app/models/material.rb
@@ -4,9 +4,11 @@ class Material < ApplicationRecord
   has_many :works, dependent: :destroy
   validates :title, presence: true
   validates :category, presence: true
+  validates :status, presence: true
   # allow_blank: true:入力がブランク時はバリデーションをスキップ
   validates :path, format: /\A#{URI::regexp(%w(http https))}\z/, allow_blank: true
   enum category:{ book: 0, video: 1, web: 2, other: 3 }
+  enum status: { learning: 0 , complete: 1 }
 
   # # joinでcurrnt_user.materials.idとworksのmaterial_idがマッチするmaterialテーブルのレコードを取得
   # # whereメソッドで条件にあったレコードを取得する
--- app/models/work.rb
+++ app/models/work.rb
@@ -3,8 +3,10 @@ class Work < ApplicationRecord
 
   validates :start, presence: true
   validates :finish, presence: true
-  validates :status, presence: true
+  validates :do_on, presence: true
+  validate :do_on_not_future_date
 
-  enum status:{ incomplete: 0, complete: 1 }
-
+  def do_on_not_future_date
+      errors.add(:do_on, "は今日以前の日付を選択してください") if do_on > Date.today
+  end
 end
