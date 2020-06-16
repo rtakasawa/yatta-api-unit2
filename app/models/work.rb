f0b7c7a2c088ef9a06c44e6ae174bef2bd884db8
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-16 18:09:26 +0900

    ページネーションのUI修正

--- app/models/work.rb
+++ app/models/work.rb
@@ -10,8 +10,6 @@ class Work < ApplicationRecord
       errors.add(:do_on, "は今日以前の日付を選択してください") if do_on.nil? || do_on > Date.today
   end
 
-  # 今月
   scope :current_month, -> { where(do_on: Date.today.beginning_of_month..Date.today.end_of_month) }
-  # 先月
   scope :last_month, -> { where(do_on: Date.today.prev_month.beginning_of_month..Date.today.prev_month.end_of_month) }
 end
