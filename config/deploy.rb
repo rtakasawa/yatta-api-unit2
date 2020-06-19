1573b92874ee4168e0321a568cf787f11219dc2d
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-19 10:54:16 +0900

    deproy-test

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,6 @@ set :deploy_to, '/var/www/yatta'
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
-set :linked_dirs, %w{node_modules}
 # 保持するバージョンの個数(※後述)
 set :keep_releases, 5
 # Rubyのバージョン
