32c94679a03a20eb99465f1762bf9baaa4915507
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-22 10:02:51 +0900

    Calendarの日付からwork登録実装中（一時コミットする）

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,7 +63,82 @@ $(function () {
         eventColor: '#63ceef',
         //イベントの文字色を変える
         eventTextColor: '#000000',
+
+        // 日付をクリックした際のイベント
+        // workの新規投稿画面に遷移したいが、現状リクエストは送信できているが、画面が描画されない
+        dayClick: function (start, end, jsEvent, view) {
+          //クリックした日付情報を取得
+          const year = moment(start).year();
+          const month = moment(start).month()+1; //1月が0のため+1する
+          const day = moment(start).date();
+          //イベント登録のためnewアクションを発火
+          $.ajax({
+            type: 'GET',
+            url: '/works/new',
+            dataType: "html"
+            // 成功処理
+          }).done(function (res) {
+            // コンソールログには描画したいhtmlが表示される。これを実際に描画する
+            console.log(res)
+            //イベント登録用のhtmlを作成
+            // $('.modal-body').html(res);
+            // //イベント登録フォームの日付をクリックした日付とする
+            // $('#event_start_time_1i').val(year);
+            // $('#event_start_time_2i').val(month);
+            // $('#event_start_time_3i').val(day);
+            //イベント登録フォームのモーダル表示
+            // $('#modal').modal();
+            // 失敗処理
+          }).fail(function (result) {
+            alert('エラーが発生しました。運営に問い合わせてください。')
+          });
+        },
+
       });
     }
   });
 });
+
+// dayClick: function(date, jsEvent, view) {
+//   //クリックした日付が取れるよ
+//   alert('Clicked on: ' + date.format());
+// },
+
+// dayClick: function(date, allDay, jsEvent, view) {
+//   /* 引数
+//   ・date(slotの依存する日付)
+//   ・allDay(全日をクリックしたかどうか全日の場合はtrue)
+//   ・jsEvent(座標などjsの情報)
+//   ・view(カレンダータイトルや表示モードの情報)
+//   */
+//   },
+
+// ここをテスト中
+// dayClick: function (start, end, jsEvent, view) {
+// $.get("/works/new",
+//     {"cd":"100", "name":"Taro"},
+//     function(dt){
+//       $().html(dt);
+// });
+// $.ajax({
+//   type: 'GET',
+//   url: '/works/new',
+//   dataType: "html"
+// });
+// //クリックした日付情報を取得
+// const year = moment(start).year();
+// const month = moment(start).month()+1; //1月が0のため+1する
+// const day = moment(start).date();
+// //イベント登録のためnewアクションを発火
+// $.ajax({
+//   type: 'GET',
+//   url: '/works/new',
+//   dataType: "html"
+// });
+
+
+// リクエストの送信はされるが、画面が変わらない
+// dayClick: function (start, end, jsEvent, view) {
+//   $.get("/works/new")
+// }
+
