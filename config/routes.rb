f9858af027ed8f9f7bb276ac53ce0a7b2080b053
ryosuke takasawa <ten.r0619@gmail.com>
2020-06-22 12:16:21 +0900

    topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'materials#index'
+  root to: 'other_pages#index'
+  # root 'materials#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   resources :materials do
     collection do
       get "search"
-      # get 'book_search'
-      # get 'qiita_search'
     end
   end
   resources :users, only: [:show]
