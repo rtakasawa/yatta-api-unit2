8d9a5088f560ab441725d011a212b4439d414552
ryosuke takasawa <ten.r0619@gmail.com>
2020-07-01 17:04:53 +0900

    letter_opnerの設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
     Bullet.rails_logger = true
   end
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web
+
 end
