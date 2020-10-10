283c61c387f7d49f84da4d67d6e68654a44f4557
ryosuke takasawa <ten.r0619@gmail.com>
2020-10-10 21:07:44 +0900

    update aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,5 +3,5 @@ ActionMailer::Base.add_delivery_method(
   AWS::SES::Base,
   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  server: 'feedback-smtp.ap-northeast-1.amazonses.com'
+  server: 'email-smtp.ap-northeast-1.amazonaws.com'
 )
