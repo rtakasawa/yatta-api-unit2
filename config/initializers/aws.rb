079b02de55a7d7532b9f9c061d57fcd5fa21b756
ryosuke takasawa <ten.r0619@gmail.com>
2020-10-12 05:35:34 +0900

    deviseの日本語化設定

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,15 +1,4 @@
-# ActionMailer::Base.add_delivery_method(
-#   :ses,
-#   AWS::SES::Base,
-#   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#   # server: 'email.us-west-2.amazonaws.com'
-#   # region: 'ap-northeast-1'
-#   # server: 'email-smtp.ap-northeast-1.amazonaws.com'
-#   server: 'email.ap-northeast-1.amazonaws.com'
-# )
-
-creds = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'],ENV['AWS_SECRET_ACCESS_KEY'],)
+creds = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'],ENV['AWS_SECRET_ACCESS_KEY'])
 
 Aws::Rails.add_action_mailer_delivery_method(
   :ses,
