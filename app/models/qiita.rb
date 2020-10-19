0e171a662c32e45d0ead814f9b61dc3e59ba732e
ryosuke takasawa <ten.r0619@gmail.com>
2020-10-20 06:46:20 +0900

    refa

--- app/models/qiita.rb
+++ app/models/qiita.rb
@@ -12,22 +12,25 @@ class Qiita
     end
 
     if response.status == 200
-      # 検索ワードにヒットした記事があった場合
-      if response.body == '[]'
-        '検索にヒットした記事がありませんでした'
-      else
-        json = JSON.parse(response.body)
-        json.sort_by { |i| i['likes_count'] }.reverse.map do |item|
-          { title: item['title'], url: item['url'], likes_count: item['likes_count'] }
-        end.take(100)
-      end
-      # Qiitaから返ってきた情報のステータスが200以外であった場合
-      # IPアドレスごとに1時間に60回までリクエスト制限あり。60回以上リクエストした場合は下記となる
+      response_success_action(response)
+    # IPアドレスごとに1時間に60回までリクエスト制限あり。60回以上リクエストした場合は下記となる
     elsif response.status == 403 && response.headers['rate-remaining'] == '0'
       '1時間に60回リクエストしましたので、利用できません。一定時間後に利用できます。'
     else
       # log/development.logにエラーログが残る
       raise "Charge failed. ErrCode: #{response.status}/ErrMessage: #{response.body}"
+    end
+  end
+
+  # Qiitaから返ってきたresponseのステータスが200であった場合
+  def self.response_success_action(response)
+    if response.body == '[]'
+      '検索にヒットした記事がありませんでした'
+    else
+      json = JSON.parse(response.body)
+      json.sort_by { |i| i['likes_count'] }.reverse.map do |item|
+        { title: item['title'], url: item['url'], likes_count: item['likes_count'] }
+      end.take(100)
     end
   end
 
