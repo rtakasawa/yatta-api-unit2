5069b8c5535b4a93152f3e8c77393b9e341ba511
ryosuke takasawa <ten.r0619@gmail.com>
2020-10-25 23:44:20 +0900

    refa user_systemspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,7 +39,17 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # Devise_confirmableのテストのため、テストの最後のトランザクションを使わないように設定する
+  config.use_transactional_fixtures = false
+
+  # database_rewinder のセットアップ
+  config.before(:suite) do
+    DatabaseRewinder.clean_all
+  end
+
+  config.after(:each) do
+    DatabaseRewinder.clean
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
