cf9bcf6d15368cd677d9242fa7167070390d7021
ryosuke takasawa <ten.r0619@gmail.com>
2021-01-01 14:45:06 +0900

    production.rbの変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 server '54.95.230.107', user: 'app', roles: %w{app db web1}
+set :ssh_options, keys: "/Users/suketa/.ssh/id_rsa"
 server '103.4.12.53', user: 'ec2-user', roles: %w{app db web2}
 set :ssh_options, keys: "/Users/suketa/.ssh/id_rsa"
