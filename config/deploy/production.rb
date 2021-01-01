edde84ab4d4432a499f0e04eeb609133a800bc40
ryosuke takasawa <ten.r0619@gmail.com>
2021-01-01 14:35:39 +0900

    capistrano:2インスタンスの自動デプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
-server '54.95.230.107', user: 'taka', roles: %w{app db web}
+server '54.95.230.107', user: 'app', roles: %w{app db web1}
+server '103.4.12.53', user: 'ec2-user', roles: %w{app db web2}
 set :ssh_options, keys: "/Users/suketa/.ssh/id_rsa"
