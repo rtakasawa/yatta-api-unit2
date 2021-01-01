54145ba58bc55700760349856786c0f50e2d0006
ryosuke takasawa <ten.r0619@gmail.com>
2021-01-02 06:03:05 +0900

    EC2インスタンスのsecondのIPアドレス変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server '54.95.230.107', user: 'app', roles: %w{app db web1}
-server '13.231.163.210', user: 'app-ec2', roles: %w{app db web2}
+server '13.113.187.149', user: 'app-ec2', roles: %w{app db web2}
 set :ssh_options, keys: "/Users/suketa/.ssh/id_rsa"
