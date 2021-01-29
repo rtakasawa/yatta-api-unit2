8684707340d0094672b4f1392b3add25060500df
ryosuke takasawa <ten.r0619@gmail.com>
2021-01-30 02:13:52 +0900

    ci refa

--- script/deploy_staging.sh
+++ script/deploy_staging.sh
@@ -4,7 +4,7 @@ set -ex
 IP=`curl -f -s ifconfig.me`
 
 aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
-ROLES=web1 bundle exec cap production deploy
+# ROLES=web1 bundle exec cap production deploy
 aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
 
 
