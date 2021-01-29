6dc228e6aab22cf384c3c14786aa0c550aa91ec1
ryosuke takasawa <ten.r0619@gmail.com>
2021-01-29 23:13:09 +0900

    refa ci

--- script/deploy_staging.sh
+++ script/deploy_staging.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION="ap-northeast-1"
-export AWS_MY_SECURITY_GROUP=${AWS_MY_SECURITY_GROUP}
 
+MYSECURITYGROUP="sg-0629db507f5c9b422"
 MYIP=`curl -s ifconfig.me`
 
-aws ec2 authorize-security-group-ingress --group-id $AWS_MY_SECURITY_GROUP --protocol tcp --port 22 --cidr $MYIP/32
+aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
 ROLES=web1 bundle exec cap production deploy
-aws ec2 revoke-security-group-ingress --group-id $AWS_MY_SECURITY_GROUP --protocol tcp --port 22 --cidr $MYIP/32
+aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
