9382046d3e527e9cd5774b71d3e57bb95bc029a7
ryosuke takasawa <ten.r0619@gmail.com>
2021-01-30 01:56:06 +0900

    ci refa

--- script/deploy_staging.sh
+++ script/deploy_staging.sh
@@ -3,10 +3,14 @@ set -ex
 
 IP=`curl -f -s ifconfig.me`
 
-trap "aws ec2 revoke-security-group-ingress --group-id ${MYSECURITYGROUP} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
-aws ec2 authorize-security-group-ingress --group-id ${MYSECURITYGROUP} --protocol tcp --port 22 --cidr ${IP}/32
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
 ROLES=web1 bundle exec cap production deploy
+aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
 
+
+# trap "aws ec2 revoke-security-group-ingress --group-id ${MYSECURITYGROUP} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
+# aws ec2 authorize-security-group-ingress --group-id ${MYSECURITYGROUP} --protocol tcp --port 22 --cidr ${IP}/32
+# ROLES=web1 bundle exec cap production deploy
 
 
 # export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
@@ -16,6 +20,6 @@ ROLES=web1 bundle exec cap production deploy
 # MYSECURITYGROUP="sg-0629db507f5c9b422"
 # MYIP=`curl -s ifconfig.me`
 
-# aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
+# aws ec2 authorize-security-group-ingress --group-id ${MYSECURITYGROUP} --protocol tcp --port 22 --cidr ${IP}/32
 # ROLES=web1 bundle exec cap production deploy
-# aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
+# aws ec2 revoke-security-group-ingress --group-id ${MYSECURITYGROUP} --protocol tcp --port 22 --cidr ${IP}/32
