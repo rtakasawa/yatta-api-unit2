c2f11cc166c31ddd32cf551fd38291d168bc9a7d
ryosuke takasawa <ten.r0619@gmail.com>
2021-01-30 01:07:48 +0900

    refa ci

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
 server '54.95.230.107', user: 'app', roles: %w{app db web1}
 server '13.113.187.149', user: 'app-ec2', roles: %w{app db web2}
-set :ssh_options, keys: "/Users/suketa/.ssh/id_rsa"
+# set :ssh_options, keys: "/Users/suketa/.ssh/id_rsa"
+set :ssh_options, {
+  keys: (ENV['PRODUCTION_SSH_KEY']),
+  forward_agent: true
+}
