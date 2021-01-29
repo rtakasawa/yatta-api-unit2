2adda47c524d1f4c503e2cd4bab585d96fda531b
ryosuke takasawa <ten.r0619@gmail.com>
2021-01-30 02:55:59 +0900

    refa ci

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '54.95.230.107', user: 'ec2-user', roles: %w{app db web1}
+server '54.95.230.107', user: 'app', roles: %w{app db web1}
 server '13.113.187.149', user: 'app-ec2', roles: %w{app db web2}
 set :ssh_options, {
   keys: (ENV['PRODUCTION_SSH_KEY']),
--- script/deploy_staging.sh
+++ script/deploy_staging.sh
@@ -7,18 +7,6 @@ trap "aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP_ID} --pr
 aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
 ROLES=web1 bundle exec cap production deploy
 
-
 # aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
 # # ROLES=web1 bundle exec cap production deploy
 # aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
-
-# export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-# export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-# export AWS_DEFAULT_REGION="ap-northeast-1"
-
-# MYSECURITYGROUP="sg-0629db507f5c9b422"
-# MYIP=`curl -s ifconfig.me`
-
-# aws ec2 authorize-security-group-ingress --group-id ${MYSECURITYGROUP} --protocol tcp --port 22 --cidr ${IP}/32
-# ROLES=web1 bundle exec cap production deploy
-# aws ec2 revoke-security-group-ingress --group-id ${MYSECURITYGROUP} --protocol tcp --port 22 --cidr ${IP}/32
